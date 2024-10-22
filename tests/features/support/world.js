e96055c78effed727e4691346519e34aed285d8f
Ironboy <ironboy@users.noreply.github.com>
2024-10-22 09:08:47 +0200

    shorthand added

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -1,4 +1,4 @@
-import seleniumWebdriver from 'selenium-webdriver';
+import seleniumWebdriver, { By } from 'selenium-webdriver';
 import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
 import { timeout, browser, headless } from '../../config.js';
 import chrome from 'selenium-webdriver/chrome.js';
@@ -6,14 +6,43 @@ import chrome from 'selenium-webdriver/chrome.js';
 const options = new chrome.Options();
 headless && options.addArguments('--headless=new');
 
+export const driver = new seleniumWebdriver
+  .Builder()
+  .setChromeOptions(options)
+  .forBrowser(browser)
+  .build();
+
 class CustomWorld {
   constructor() {
-    this.driver = new seleniumWebdriver
-      .Builder()
-      .setChromeOptions(options)
-      .forBrowser(browser)
-      .build();
+    this.driver = driver;
   }
+
+  async get(cssSelector) {
+    return await this.driver.findElement(By.css(cssSelector));
+  }
+
+  async getMany(cssSelector) {
+    return await this.driver.findElements(By.css(cssSelector));
+  }
+
+  async getByXPath(xPath) {
+    return await this.driver.findElement(By.xpath(xPath));
+  }
+
+  async getManyByXPath(xPath) {
+    return await this.driver.findElements(By.xpath(xPath));
+  }
+
+  async getWait(cssSelector, maxTimeToWaitMs = 5000) {
+    return await this.driver.wait(
+      until.elementLocated(By.css(cssSelector)), maxTimeWaitMs);
+  }
+
+  async getByXPathWait(xPath, maxTimeToWaitMs = 5000) {
+    return await this.driver.wait(
+      until.elementLocated(By.xpath(xPath)), maxTimeWaitMs);
+  }
+
 }
 
 setDefaultTimeout(timeout);
